a981014e61ad9cd02505d195186099faa4ce2af8
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-30 17:04:13 +0200

    modificaciones de database para el dataset

--- gestor/database.py
+++ gestor/database.py
@@ -1,3 +1,5 @@
+import csv
+
 class Cliente:
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
@@ -9,6 +11,11 @@ class Cliente:
 class Clientes:
     # Lista de clientes
     lista = []
+    with open("clientes.csv", newline="\n") as fichero:
+        reader = csv.reader(fichero, delimiter=";")
+        for dni, nombre, apellido in reader:
+            cliente = Cliente(dni, nombre, apellido)
+            lista.append(cliente)
     
     @staticmethod
     def buscar(dni):
@@ -20,6 +27,7 @@ class Clientes:
     def crear(dni, nombre, apellido):
             cliente = Cliente(dni, nombre, apellido)
             Clientes.lista.append(cliente)
+            Clientes.guardar() 
             return cliente
  
     @staticmethod
@@ -28,6 +36,7 @@ class Clientes:
             if cliente.dni == dni:
                 Clientes.lista[i].nombre = nombre
                 Clientes.lista[i].apellido = apellido
+                Clientes.guardar() 
                 return Clientes.lista[i]
  
     @staticmethod
@@ -35,4 +44,12 @@ class Clientes:
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 cliente = Clientes.lista.pop(i)
+                Clientes.guardar() 
                 return cliente
+    
+    @staticmethod
+    def guardar():
+         with open("clientes.csv", "w", newline="\n") as fichero:
+            writer = csv.writer(fichero, delimiter=";")
+            for c in Clientes.lista:
+                writer.writerow((c.dni, c.nombre, c.apellido))
