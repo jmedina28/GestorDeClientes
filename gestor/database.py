3ce2e08735435e43da49c85bb0b903395666b4f7
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-30 17:24:30 +0200

    mas modificaciones

--- gestor/database.py
+++ gestor/database.py
@@ -5,51 +5,56 @@ class Cliente:
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
+
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
 
+    def to_dict(self):
+        return {'dni': self.dni, 'nombre': self.nombre, 'apellido': self.apellido}
+
+
 class Clientes:
-    # Lista de clientes
+
     lista = []
-    with open(config.DATABASE_PATH, newline="\n") as fichero:
-        reader = csv.reader(fichero, delimiter=";")
+    with open(config.DATABASE_PATH, newline='\n') as fichero:
+        reader = csv.reader(fichero, delimiter=';')
         for dni, nombre, apellido in reader:
             cliente = Cliente(dni, nombre, apellido)
             lista.append(cliente)
-    
+
     @staticmethod
     def buscar(dni):
         for cliente in Clientes.lista:
             if cliente.dni == dni:
                 return cliente
-    
+
     @staticmethod
     def crear(dni, nombre, apellido):
-            cliente = Cliente(dni, nombre, apellido)
-            Clientes.lista.append(cliente)
-            Clientes.guardar() 
-            return cliente
- 
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        Clientes.guardar()
+        return cliente
+
     @staticmethod
     def modificar(dni, nombre, apellido):
-        for i, cliente in enumerate(Clientes.lista):
+        for indice, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                Clientes.lista[i].nombre = nombre
-                Clientes.lista[i].apellido = apellido
-                Clientes.guardar() 
-                return Clientes.lista[i]
- 
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido = apellido
+                Clientes.guardar()
+                return Clientes.lista[indice]
+
     @staticmethod
     def borrar(dni):
-        for i, cliente in enumerate(Clientes.lista):
+        for indice, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                cliente = Clientes.lista.pop(i)
-                Clientes.guardar() 
+                cliente = Clientes.lista.pop(indice)
+                Clientes.guardar()
                 return cliente
-    
+
     @staticmethod
     def guardar():
-         with open(config.DATABASE_PATH, "w", newline="\n") as fichero:
-            writer = csv.writer(fichero, delimiter=";")
-            for c in Clientes.lista:
-                writer.writerow((c.dni, c.nombre, c.apellido))
+        with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
--- gestor/tests/test_database.py
+++ gestor/tests/test_database.py
@@ -1,24 +1,24 @@
+import csv
 import copy
+import config
+import helpers
 import unittest
 import database as db
-import helpers
-import config
-import csv
 
 class TestDatabase(unittest.TestCase):
 
     def setUp(self):
         db.Clientes.lista = [
-        db.Cliente('15J', 'Marta', 'Pérez'),
-        db.Cliente('48H', 'Manolo', 'López'),
-        db.Cliente('28Z', 'Ana', 'García')
+            db.Cliente('15J', 'Marta', 'Pérez'),
+            db.Cliente('48H', 'Manolo', 'López'),
+            db.Cliente('28Z', 'Ana', 'García')
         ]
 
     def test_buscar_cliente(self):
         cliente_existente = db.Clientes.buscar('15J')
-        cliente_no_existente = db.Clientes.buscar('99X')
+        cliente_inexistente = db.Clientes.buscar('99X')
         self.assertIsNotNone(cliente_existente)
-        self.assertIsNone(cliente_no_existente)
+        self.assertIsNone(cliente_inexistente)
 
     def test_crear_cliente(self):
         nuevo_cliente = db.Clientes.crear('39X', 'Héctor', 'Costa')
@@ -29,33 +29,30 @@ class TestDatabase(unittest.TestCase):
 
     def test_modificar_cliente(self):
         cliente_a_modificar = copy.copy(db.Clientes.buscar('28Z'))
-        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana',
-    'Pérez')
+        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana', 'García')
         self.assertEqual(cliente_a_modificar.nombre, 'Ana')
         self.assertEqual(cliente_modificado.nombre, 'Mariana')
 
     def test_borrar_cliente(self):
         cliente_borrado = db.Clientes.borrar('48H')
         cliente_rebuscado = db.Clientes.buscar('48H')
-        self.assertNotEqual(cliente_borrado, cliente_rebuscado)
-    
+        self.assertEqual(cliente_borrado.dni, '48H')
+        self.assertIsNone(cliente_rebuscado)
+
     def test_dni_valido(self):
         self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
-        self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('232323S', db.Clientes.lista))
         self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
         self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
-    
+
     def test_escritura_csv(self):
         db.Clientes.borrar('48H')
         db.Clientes.borrar('15J')
-        db.Clientes.modificar('28Z', 'Mariana', 'Pérez')
+        db.Clientes.modificar('28Z', 'Mariana', 'García')
         dni, nombre, apellido = None, None, None
-        with open(config.DATABASE_PATH, newline="\n") as fichero:
-            reader = csv.reader(fichero, delimiter=";")
-            dni, nombre, apellido = next(reader) # Primera línea del iterador
+        with open(config.DATABASE_PATH, newline='\n') as fichero:
+            reader = csv.reader(fichero, delimiter=';')
+            dni, nombre, apellido = next(reader)
         self.assertEqual(dni, '28Z')
         self.assertEqual(nombre, 'Mariana')
-        self.assertEqual(apellido, 'Pérez')
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(apellido, 'García')
