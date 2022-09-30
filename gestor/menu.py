ffa1110ae526d24b3dcdaca8662a43bf7dd6e025
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-30 16:59:45 +0200

    modificada la funcion de añadir clientes

--- gestor/menu.py
+++ gestor/menu.py
@@ -32,14 +32,16 @@ def iniciar():
         
         if opcion == '3':
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(
-                3, 3, "DNI (2 ints y 1 char)").upper()
+            # Comprobación de DNI válido
+            while 1:
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
             nombre = helpers.leer_texto(
-                2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+            2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(
-                2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente.")
+            2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+            db.Clientes.crear(dni, nombre, apellido)  
         
         if opcion == '4':
             print("Modificando un cliente...\n")
